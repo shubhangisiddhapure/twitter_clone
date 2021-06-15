fe834e6b531187e436f00aa4f5c85e7999396180
Shubhangi <shubhangis20@navgurukul.org>
2021-06-15 23:46:49 +0530

    search box added

--- backend/src/route/user.js
+++ backend/src/route/user.js
@@ -17,5 +17,42 @@ router.get("/myProfile", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+router.get("/allusers", async (req, res) => {
+  try {
+    const users = await User.find().populate({
+      path: "user",
+      select: "fullname",
+    });
+    if (users.length === 0) {
+      return res.status(404).json({ msg: "users not found" });
+    }
+    res.status(200).json({ msg: "All users", data: users });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("sever error");
+  }
+});
+router.get(
+  "/userprofile",
+  [check("username", "please enter username").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { username } = req.body;
+    try {
+      let user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: "user not found" }] });
+      } else {
+        res.status(200).json({ msg: "user", data: user });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("sever error");
+    }
+  }
+);
 
 module.exports = router;
--- frontend/src/container/navbar.js
+++ frontend/src/container/navbar.js
@@ -1,23 +1,83 @@
 /** @format */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import axios from "axios";
 const NavBar = () => {
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [userdata, setUserdata] = useState("");
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get(
+        "http://localhost:7000/api/user/allusers"
+      );
+      setUsers(response.data.data);
+    };
+    loadUsers();
+  }, []);
+  const onSuggestHandler = async (text) => {
+    setUserdata(true);
+    setText(text);
+    setSuggestions([]);
+    const data = text;
+    console.log(data);
+    const profile = await axios.get(
+      "http://localhost:7000/api/user/userprofile",
+      { data }
+    );
+    console.log(profile);
+  };
+  const onChangeHandler = (text) => {
+    let matches = [];
+    if (text.length >= 1) {
+      matches = users.filter((user) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return user.username.match(regex);
+      });
+    }
+    console.log(matches);
+    setSuggestions(matches);
+    setText(text);
+  };
   return (
-    <Navbar style={{backgroundColor:"#50b7f5"}}>
-      <Nav className="mr-auto">
-        <Nav.Link href="/profile">My Profile</Nav.Link>
-      </Nav>
-      <Nav className="mr-auto">
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Nav>
-      <Nav className="ml-auto">
-        <Nav.Link href="/">Logout</Nav.Link>
-      </Nav>
-    </Navbar>
+    <div>
+      <Navbar style={{ backgroundColor: "#50B7F5" }}>
+        <Nav className="mr-auto">
+          <Nav.Link href="/profile">My Profile</Nav.Link>
+        </Nav>
+        <Nav className="mr-auto">
+          <Form>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              value={text}
+              onChange={(e) => onChangeHandler(e.target.value)}
+              //  onBlur={()=>{
+              //    setTimeout(()=>{
+              //       setSuggestions([])
+              //    },100)
+              //  }}
+            />
+            {suggestions &&
+              suggestions.map((suggestion, i) => (
+                <div
+                  key={i}
+                  className="suggestion mr-sm-2"
+                  onClick={() => onSuggestHandler(suggestion.username)}
+                >
+                  {suggestion.username}
+                </div>
+              ))}
+          </Form>
+        </Nav>
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Logout</Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
 export default NavBar;
